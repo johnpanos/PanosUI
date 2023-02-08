35fb341e8b3e65beb1a8aee426168e9dc5aca1c2
John Panos <johncpanos@gmail.com>
2023-02-08 09:29:16 -0800

    Fix tests

--- tests/test_UIView.c
+++ tests/test_UIView.c
@@ -9,14 +9,14 @@ int main()
         .width = 100,
         .height = 100};
 
-    UIView *view1 = UIViewCreate(rect, rect);
+    UIView view1 = UIViewCreate(rect, rect);
     assert(view1 != NULL);
     assert(view1->frame.x == rect.x);
     assert(view1->frame.y == rect.y);
     assert(view1->frame.width == rect.width);
     assert(view1->frame.height == rect.height);
 
-    UIView *view2 = UIViewCreate(rect, rect);
+    UIView view2 = UIViewCreate(rect, rect);
     assert(view2 != NULL);
     assert(view2->frame.x == rect.x);
     assert(view2->frame.y == rect.y);
@@ -26,7 +26,7 @@ int main()
     UIViewAddSubview(view1, view2);
     assert(view2->parentView == view1);
 
-    UIView *view3 = UIViewCreate(rect, rect);
+    UIView view3 = UIViewCreate(rect, rect);
     UIViewAddSubview(view1, view3);
 
     UIViewRemoveSubview(view1, view3);
