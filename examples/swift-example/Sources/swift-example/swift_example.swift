0d0728c6e06a80779fc0f7b5e22b938baf731c31
John Panos <johncpanos@gmail.com>
2023-02-16 10:42:48 -0800

    Update swift example

--- examples/swift-example/Sources/swift-example/swift_example.swift
+++ examples/swift-example/Sources/swift-example/swift_example.swift
@@ -17,13 +17,11 @@ protocol UIApplicationDelegate {
     func didFinishLaunching()
 }
 
-
-
 struct MyDelegate: UIApplicationDelegate {
     func didFinishLaunching() {
         print("Did finish from Swift!")
 
-        let rect = UIRectCreate(0, 0, 100, 100)
+        let rect = UIRectCreate(0, 0, 750, 600)
         let win = UIWindowCreate(rect)
 
         let windowController = UIWindowControllerCreate()
@@ -33,6 +31,22 @@ struct MyDelegate: UIApplicationDelegate {
             
             UIViewSetBackgroundColor(myView, UIColorCreateRGBA(0, 0, 0, 255))
             UIViewAddSubview(window!.pointee.mainView, myView)
+
+            var start: Float = 100.0
+            var end: Float = 500.0
+
+            var myAnim: UIAnimation = UIAnimation()
+            myAnim.finished = 0
+            myAnim.forKey = kUILayerKeyPositionX
+            myAnim.timingFunction = UIAnimationTimingFunctionEaseInOutCubic
+            myAnim.startValue = .init(&start)
+            myAnim.endValue = .init(&end)
+            myAnim.startTime = UIAnimationGetCurrentTime()
+            myAnim.endTime = UIAnimationGetCurrentTime() + 2500
+            myAnim._valueSize = MemoryLayout<Float>.size
+            myAnim.duration = 2500
+
+            UILayerAddAnimation(myView!.pointee.layer, myAnim)
         }
 
         win!.pointee.controller = windowController
